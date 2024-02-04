928a38fad08fd2a17a140b248f3ceadc28822813
gaYoung95 <158826352+gaYoung95@users.noreply.github.com>
2024-02-04 23:30:12 +0900

    Add files via upload

---
+++ Test004.java
@@ -0,0 +1,26 @@
+public class Test004
+{
+ public static void main(String[] args)
+	{
+		
+		String name="최혜인";
+		String tel="0102509183";
+		String 주소="경기도 남양주시";
+
+		System.out.println("name : " + name);
+		System.out.println("tel : " + $tel);
+		System.out.println("주소 : " + 주소);
+		
+
+		//long tel = 0102509183;  숫자로만 구성된 데이터라 할지라도 맨 앞에 0이 들어올 수 있는 구조라면 숫자 형태로 처리해서는 안 된다. 
+	
+	}
+}
+
+/*
+name : 최혜인
+_tel : 0102509183
+주소 : 경기도 남양주시
+계속하려면 아무 키나 누르십시오 . . .
+
+*/
